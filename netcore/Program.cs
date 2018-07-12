6ff686846ffb43a35186dfed440f317c14d2419f
Eason Kang <eason.kang@autodesk.com>
2018-07-12 23:20:01 +0800

    First Commit

---
+++ netcore/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Linq;
+
+namespace Autodesk
+{
+    class Program
+    {
+        static void TestCall()
+        {
+            //specify to use TLS 1.2 as default connection
+            //System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var handler = new HttpClientHandler();
+            handler.SslProtocols = SslProtocols.Tls12;
+
+            var httpClient = new HttpClient(handler);
+            var requestMessage = new HttpRequestMessage();
+            
+            var uri = new Uri("https://developer.api.autodesk.com/project/v1/hubs?filter[extension.type]=hubs:autodesk.bim360:Account");
+            requestMessage.Headers.Host = uri.Authority;
+            requestMessage.RequestUri = uri;
+            requestMessage.Method = HttpMethod.Get;
+            requestMessage.Headers.Add("Authorization", string.Format("Bearer {0}", "YOUR_ACCESS_TOKEN"));
+
+            var task = httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead);
+
+            var result = task.Result;
+            var json = result.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(json);
+        }
+
+        static void Main(string[] args)
+        {
+            TestCall();
+        }
+    }
+}
---
+++ php/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://developer.api.autodesk.com/project/v1/hubs?filter[extension.type]=hubs:autodesk.bim360:Account",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_2,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Bearer YOUR_ACCESS_TOKEN",
+    "Cache-Control: no-cache",
+    "Content-Type: application/vnd.api+json"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+$curl_info = curl_version();
+echo $curl_info['ssl_version']. "\n";
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
